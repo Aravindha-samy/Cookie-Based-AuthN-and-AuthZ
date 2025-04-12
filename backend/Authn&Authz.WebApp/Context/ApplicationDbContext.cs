9f22b2b831125d3c94c9c355f88aa704d7db62f2
Aravindha-samy <Aravindha Samy Balakrishnan>
2025-04-12 11:59:39 +0530

    frontend and backend Initial Commit

---
+++ backend/Authn&Authz.WebApp/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authn_Authz.WebApp.Context
+{
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+      
+    }
+}
